955078af696389ae91a5229c34863eacbdbfdc74
Pedro16-A <82624521+Pedro16-A@users.noreply.github.com>
2021-05-02 19:12:59 -0400

    Add files via upload

---
+++ tarea02/empleado/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace empleado
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            Empleado empleado1 = new Empleado("Pedro Silva",28256628,"control de calidad",200);
+
+            Console.WriteLine(empleado1.getInfoempleado());
+        
+        }
+    }
+
+    class Empleado{
+        public Empleado(string nombre1,int cedula1,string departamento1,int salario1){
+            nombre = nombre1;
+            cedula = cedula1;
+            departamento = departamento1;
+            salario = salario1;
+        }
+
+        public String getInfoempleado(){
+
+            return " Informacion del empleado: " + nombre + "Cedula " + cedula + "Departamento " + departamento + "Salario " + salario;
+
+        }
+
+        private String nombre;
+        private int cedula;
+
+        private String departamento;
+
+        private int salario;
+        }
+}
