265b83a37a8847cc704601f6137f6afa0035b1da
Pedro16-A <82624521+Pedro16-A@users.noreply.github.com>
2021-05-26 11:18:09 -0400

    Add files via upload

---
+++ figuras/figuras_/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace figuras_
+{
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo miRectangulo = new Rectangulo();
+
+            Console.WriteLine("Ingrese la base del rectangulo:");
+
+            miRectangulo.base1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la altura del rectangulo:");
+
+            miRectangulo.altura = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(miRectangulo.calculoArea());
+
+        }
+        
+        class Rectangulo
+        {
+            public int base1;
+
+            public int altura;
+
+            int area;
+
+
+            public int calculoArea()
+            {
+                return area = base1 * altura;
+            }
+
+        }
+}
+}
